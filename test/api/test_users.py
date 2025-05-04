cb9e1ff712c1506ad0acbce7e6bbd30c9d586f36
Nolan Knievel <nolan.knievel@icloud.com>
2025-05-04 16:47:28 -0700

    add users tests

--- test/api/test_users.py
+++ test/api/test_users.py
@@ -1,4 +1,4 @@
-from src.api.inventory import get_capacity_plan, create_capacity_plan, CapacityPlan, deliver_capacity_plan
+from src.api.users import create_new_user
 
 import sqlalchemy
 from src.api import auth
@@ -7,4 +7,33 @@ from src import database as db
 
 
 def test_users():
-    pass
+    response = create_new_user("testuser")
+
+    with db.engine.begin() as connection:
+        result = connection.execute(
+            sqlalchemy.text("SELECT username FROM users WHERE username = :username"),
+            {"username": "testuser"}
+        ).one()
+
+    assert result.username == "testuser"
+    assert response.status_code == 204
+
+    response = create_new_user("testuser")
+    assert response.status_code == 409
+
+    create_new_user("testuser3")
+    create_new_user("testuser4")
+    create_new_user("testuser5")
+
+
+    with db.engine.begin() as connection:
+    result = connection.execute(
+        sqlalchemy.text("SELECT username FROM users")
+    ).fetchall()
+
+    usernames = set([row.username for row in result])
+
+    assert usernames == {"testuser", "testuser3", "testuser4", "testuser5"}
+    
+
+    
