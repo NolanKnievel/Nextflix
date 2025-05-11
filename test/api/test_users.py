007788be80725dcdac7290493cb7106bb5578982
Nolan Knievel <nolan.knievel@icloud.com>
2025-05-11 11:54:42 -0700

    get add to watchlist route working

--- test/api/test_users.py
+++ test/api/test_users.py
@@ -1,4 +1,4 @@
-from src.api.users import create_new_user
+from src.api.users import create_new_user, add_to_watchlist
 
 import sqlalchemy
 from src.api import auth
@@ -44,3 +44,105 @@ def test_users():
     
 
     
+def test_add_to_watchlist():
+    # create a new user
+    create_new_user("testuser33")
+
+    # insert a media item into the media table
+    with db.engine.begin() as connection:
+        media_id = connection.execute(
+            sqlalchemy.text(
+                """
+                INSERT INTO media (media_type, title, director) VALUES (:media_type, :title, :director)
+                RETURNING media_id
+                """
+            ),
+            {
+                "media_type": "movie",
+                "title": "Test Movie",
+                "director": "Test Director"
+            }
+        ).scalar_one()
+
+        connection.execute(
+            sqlalchemy.text(
+                """
+                INSERT INTO movies (media_id, length) VALUES (:media_id, :length)
+                """
+            ),
+            {
+                "media_id": media_id,
+                "length": 120
+            }
+        )
+
+    # add to watchlist
+    add_to_watchlist("testuser", "Test Movie", have_watched=False)
+
+    # check if the entry was added to the watchlist
+    with db.engine.begin() as connection:
+        result = connection.execute(
+            sqlalchemy.text(
+                """
+                SELECT * FROM watchlists
+                WHERE user_id = (SELECT id FROM users WHERE username = :username)
+                AND media_id = :media_id
+                """
+            ),
+            {
+                "username": "testuser",
+                "media_id": media_id
+            }
+        ).fetchone()
+
+    assert result is not None
+
+    # tear down
+    # delete the movie from the movies table
+    with db.engine.begin() as connection:
+        connection.execute(
+            sqlalchemy.text(
+                """
+                DELETE FROM movies WHERE media_id = :media_id
+                """
+            ),
+            {
+                "media_id": media_id
+            }
+        )
+        # delete the movie watchlist
+        connection.execute(
+            sqlalchemy.text(
+                """
+                DELETE FROM watchlists WHERE user_id = (SELECT id FROM users WHERE username = :username)
+                AND media_id = :media_id
+                """
+            ),
+            {
+                "username": "testuser",
+                "media_id": media_id
+            }
+        )
+        # delete the user from the users table
+        connection.execute(
+            sqlalchemy.text(
+                """
+                DELETE FROM users WHERE username = :username
+                """
+            ),
+            {
+                "username": "testuser"
+            }
+        )
+        # delete the media from the media table
+        connection.execute(
+            sqlalchemy.text(
+                """
+                DELETE FROM media WHERE media_id = :media_id
+                """
+            ),
+            {
+                "media_id": media_id
+            }
+        )
+
